1f523618ef9d816e2cd3c7f15519b146526f8941
manoj-humber <n01418411@humbermail.ca>
2023-03-28 00:59:17 -0400

    upadte

---
+++ core_java/jdbc/L_JDBC/src/main/java/com/manoj/jdbc/JDBCExecutor.java
@@ -0,0 +1,13 @@
+package com.manoj.jdbc;
+
+/**
+ * Hello world!
+ *
+ */
+public class JDBCExecutor
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
